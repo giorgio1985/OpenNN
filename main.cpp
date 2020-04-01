346e68db269e252b5b16872d8063c248f7af4c22
giorgio1985 <g.adonoo@yahoo.it>
2020-04-01 22:25:21 +0200

    add libraries

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include<stdlib.h>
 #include<string>
 #include<iostream>
+//#include "data_set.h"
+//#include "metrics.h"
 
 int main(int argc, char const *argv[])
 {
-	std :: cout<<"its made..";
+	std :: cout<<"its made\n";
 	return 0;
 }
