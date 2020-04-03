8ca4e619fdca32bc8daed0b1d5504d1042500c03
giorgio1985 <g.adonoo@yahoo.it>
2020-04-03 22:45:45 +0200

    try to bug resolution libraries

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
-#include<stdlib.h>
-#include<string>
-#include<iostream>
-#include "vector.h"
-#include "data_set.h"
-#include "matrix.h"
-#include "vector.h"
+#include <stdlib.h>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "opennn_4.0/opennn/opennn.h"
+#include "opennn_4.0/opennn/data_set.h"
 
-int main(int argc, char const *argv[])
+using namespace OpenNN;
+using namespace std;
+
+int main(void)
 {
-	std :: cout<<"its made\n";
-	DataSet :: DataSet data_set("iris_flowers.csv",',',true);
+	 
+
+   std :: cout<<"its made\n";
+    
+   DataSet data_set;
+   data_set("iris_flowers.csv",',', true);
+   data_set.set_columns_uses({"Input", "Input", "Input", "Input", "Target"});
+
 	return 0;
 }
